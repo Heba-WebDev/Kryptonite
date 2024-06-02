920899696370aff43e19632ac000c314235699bb
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-02 04:43:55 +0300

    Added api property for better ux in the swagger docs

--- src/users/dto/email.user.dto.ts
+++ src/users/dto/email.user.dto.ts
@@ -1,6 +1,12 @@
 import { IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class EmailUserDto {
+  @ApiProperty({
+    example: 'test@example.com',
+    description: 'The email of the user',
+    nullable: false,
+  })
   @IsEmail()
   readonly email: string;
 }
