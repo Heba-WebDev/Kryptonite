6d8eb1aeecb47ddd01bfd4f022b777a557b7cf8d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-02 20:51:48 +0300

    Added test cases to test register controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { prismaMock } from '../../test/mocks/prisma.mock';
+import { userMock } from '../../test/mocks/user.mock';
+import { userServiceMock } from '../../test/mocks/userService.mock';
+import { EmailUserDto } from './dto/email.user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +13,16 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: userServiceMock,
+        },
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +30,26 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('create => should create a new user by a given data', async () => {
+      const user = {
+        email: userMock.email,
+      } as EmailUserDto;
+
+      jest.spyOn(userServiceMock, 'register').mockReturnValue(user);
+
+      const result = await controller.register(user);
+
+      expect(userServiceMock.register).toHaveBeenCalled();
+      expect(userServiceMock.register).toHaveBeenCalledWith(user);
+
+      expect(result).toEqual(user);
+    });
+  });
+
+  describe('login', () => {
+    it('should log in a user successfully', async () => {});
   });
 });
