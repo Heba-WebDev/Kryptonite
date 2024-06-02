6871454e163cf4127ee52ae62527908bc903d1db
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-03 02:35:18 +0300

    Added prisma, usersSerivce and cloudinary as providers

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { CloudinaryProvider } from './cloudinary.provider';
 
 @Module({
-  providers: [FilesService],
-  controllers: [FilesController]
+  providers: [FilesService, PrismaService, UsersService, CloudinaryProvider],
+  controllers: [FilesController],
 })
 export class FilesModule {}
