9e1b7a2303d504ab484fe5aa0b855ca7a4df7786
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-20 21:07:29 +0300

    fix: import issues

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { UsersService } from 'src/users/users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { UsersService } from '../users/users.service';
 import { CloudinaryProvider } from './cloudinary.provider';
 
 @Module({
