1a9a2833619363bdd48117822111995d37e88f4c
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-16 21:19:29 +0300

    feat: updated user mock to include files and otp

--- test/mocks/user.mock.ts
+++ test/mocks/user.mock.ts
@@ -1,8 +1,10 @@
-import { randomUUID } from 'crypto';
+import { User } from 'src/entities/user.entity';
 
-export const userMock = {
-  id: randomUUID,
+export const userMock: User = {
+  id: 'randomUUID',
   email: 'example@exmaple.com',
   api_key: '' || 'sfssgs',
   is_verified: true || false,
+  files: [],
+  otp: [],
 };
