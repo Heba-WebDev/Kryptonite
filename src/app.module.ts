513d5746067263ec6bda9907b12e58d2e629e73b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-16 21:11:14 +0300

    feat: added postgres configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { File } from './entities/file.entity';
+import { OTP } from './entities/otp.entity';
 
 @Module({
-  imports: [UsersModule, FilesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5433,
+        username: 'user',
+        password: 'pass123',
+        database: 'mydb',
+        entities: [User, File, OTP],
+        synchronize: true,
+      }),
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
