713968ffc4ecd5561fa7ca0ea2c2ee17e03841dc
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-16 21:18:29 +0300

    feat: imported user, otp and file entities

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { OTP } from 'src/entities/otp.entity';
+import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService, PrismaService],
+  imports: [TypeOrmModule.forFeature([User, OTP])],
+  providers: [UsersService],
   controllers: [UsersController],
 })
 export class UsersModule {}
