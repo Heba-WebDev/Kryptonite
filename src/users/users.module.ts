2ee54dbdc5db3e2d08e62a3739afda9ab2a0ca7f
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-17 00:58:16 +0300

    feat: added command handlers and query handlers to users module providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { OTP } from 'src/entities/otp.entity';
 import { UsersService } from './users.service';
+import { CqrsModule } from '@nestjs/cqrs';
+import { RegisterUserHandler } from './handlers/register-user.handler';
+import { EmailExistsQuery } from './queries/email-exists.query';
+import { GetUserQuery } from './queries/get-user.query';
+import { LoginUserHandler } from './handlers/login-user.handler';
+const CommandHandlers = [LoginUserHandler, RegisterUserHandler];
+const QueryHandlers = [EmailExistsQuery, GetUserQuery];
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, OTP])],
-  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([User, OTP]), CqrsModule],
+  providers: [UsersService, ...CommandHandlers, ...QueryHandlers],
   controllers: [UsersController],
 })
 export class UsersModule {}
